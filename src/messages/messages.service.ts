60261a5f5e4ced043684c8e23b97b48675cdd23d
Falola Ibrahim <ayofalola240@gmail.com>
2022-03-08 06:37:20 +0100

    Implement dependency injection

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,17 @@
+import { MessagesRepository } from './messages.repository';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MessagesService {
+  constructor(public messagesRepo: MessagesRepository) {}
+
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
