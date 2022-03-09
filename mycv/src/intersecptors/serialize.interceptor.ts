7a25da0fe24384043c585235ce9f5f6e58df55b1
Falola Ibrahim <ayofalola240@gmail.com>
2022-03-09 17:53:52 +0100

    New updates

---
+++ mycv/src/intersecptors/serialize.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // Run something before a request is handled
+    //by the request handler
+    // console.log('Im running before the handler', context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        // console.log('Im running before response is sent out', data);
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
